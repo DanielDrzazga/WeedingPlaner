81af75273d54461a7f758457f270f6d0a42c7397
Daniel Drzazga <whater.d2@gmail.com>
2020-10-26 21:34:51 +0100

    created security basic configuration

---
+++ src/main/java/weddingplanner/login/component/security/config/SecurityConfig.java
@@ -0,0 +1,81 @@
+package weddingplanner.login.component.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import weddingplanner.application.component.MD5Encoder;
+
+import javax.sql.DataSource;
+
+/**
+ * Create by Daniel Drzazga on 16.10.2020
+ **/
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private DataSource dataSource;
+    private MD5Encoder md5Encoder;
+
+    public SecurityConfig(DataSource dataSource) {
+        this.md5Encoder = new MD5Encoder();
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable().
+                headers().frameOptions().sameOrigin()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/admin/users/new")
+                    .permitAll()
+                    .antMatchers("/")
+                    .authenticated()
+                .and()
+                    .formLogin()
+                    .usernameParameter("email")
+                    .passwordParameter("password")
+                    .permitAll()
+                .and().
+                    exceptionHandling()
+                .and()
+                    .logout()
+                    .invalidateHttpSession(true)
+                .deleteCookies("JSESSIONID");
+    }
+
+
+    @Autowired
+    public void configAuthentication(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .jdbcAuthentication()
+                .usersByUsernameQuery("SELECT email, password, active FROM users WHERE email=?")
+                .authoritiesByUsernameQuery("SELECT u.email, r.role_name FROM users_roles ur LEFT JOIN users u on u.id=ur.user_id LEFT JOIN roles r on r.id=ur.role_id WHERE email=?")
+                .dataSource(dataSource)
+                .passwordEncoder(passwordEncoder());
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new PasswordEncoder() {
+            @Override
+            public String encode(CharSequence charSequence) {
+                return md5Encoder.getMD5Hash(charSequence.toString());
+            }
+
+            @Override
+            public boolean matches(CharSequence charSequence, String encodedPassword) {
+                return md5Encoder.getMD5Hash((charSequence.toString())).equals(encodedPassword);
+            }
+        };
+    }
+
+
+}
