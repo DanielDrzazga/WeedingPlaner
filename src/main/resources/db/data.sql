496b6f09661928e32039e2be3af34a2835f3ad02
Daniel Drzazga <whater.d2@gmail.com>
2020-10-26 21:27:25 +0100

    added data source class path to application.properties created data.sql with query to add user,admin role to database

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,3 @@
+insert into roles (ID ,ACTIVE, DESCRIPTION, ROLE_NAME) values ('1', 'FALSE', 'null', 'ADMIN');
+insert into roles (ID ,ACTIVE, DESCRIPTION, ROLE_NAME) values ('2', 'FALSE', 'null', 'USER');
+
