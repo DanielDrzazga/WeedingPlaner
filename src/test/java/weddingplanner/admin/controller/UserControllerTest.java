8987e8b25c5d6e871833c0c7e5ffee9775f71b04
Daniel Drzazga <whater.d2@gmail.com>
2020-10-26 21:35:36 +0100

    fic application context error

--- src/test/java/weddingplanner/admin/controller/UserControllerTest.java
+++ src/test/java/weddingplanner/admin/controller/UserControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.OverrideAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
@@ -26,6 +27,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest(UserController.class)
+@OverrideAutoConfiguration(enabled=true)
 class UserControllerTest {
 
     @Autowired
